552513a54ae6733d732800150e3f231707241986
kangbok-lee <gangbok119@gmail.com>
2019-03-18 16:24:35 +0900

    routes 설정, 컨트롤러는 일단 routes에 통합

---
+++ api/routes/orders.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Orders were fetched'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Order was created'
+    });
+});
+
+router.get('/:orderId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Order deleted',
+        orderId: req.params.orderId
+    });
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -4,11 +4,18 @@ const express = require('express');
 // express 기능 사용 설정
 const app = express();
 
-app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'It works!'
-    });
-});
+// routes로 처리하므로 root는 주석처리
+// app.use((req, res, next) => {
+//     res.status(200).json({
+//         message: 'It works!'
+//     });
+// });
+
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
 
 // module화 하여 export하기 
 module.exports = app;
