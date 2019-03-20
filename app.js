2064b311861ffafaeaecdb89ba37221493bdbcee
kangbok-lee <gangbok119@gmail.com>
2019-03-20 19:26:49 +0900

    mongoose 추가, 설정

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ const app = express();
 // logger 추가
 const morgan = require('morgan');
 
+// req 바디 파서 추가
+const bodyParser = require('body-parser');
+
+
+// mongoose 추가
+const mongoose = require('mongoose');
+
+// router 모듈 추가
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+const db = require('./config/key').mongoURI;
 // routes로 처리하므로 root는 주석처리
 // app.use((req, res, next) => {
 //     res.status(200).json({
@@ -13,10 +25,26 @@ const morgan = require('morgan');
 //     });
 // });
 
-app.use(morgan('dev'));
+mongoose.connect(db, { useNewUrlParser: true})
+    .then(() => console.log("MongoDB Connected ..."))
+    .catch(err => console.log(err));
 
-const productRoutes = require('./api/routes/products');
-const orderRoutes = require('./api/routes/orders');
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
---
+++ config/key.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: "mongodb+srv://gangbok119:0331RKDqhr@cluster0-5ont9.mongodb.net/test?retryWrites=true"
+};
