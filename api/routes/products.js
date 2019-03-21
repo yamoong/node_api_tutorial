753ed9f6885e50082ce00e53a0c681c36d0579fa
kangbok-lee <gangbok119@gmail.com>
2019-03-21 18:36:24 +0900

    추가 모델, 라우터 및 컨트롤러 수정

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,11 +5,13 @@ const mongoose = require('mongoose');
 // MongoDB 모델 스키마 추가
 const Product = require('../models/product')
 
+
 router.get('/', (req, res, next) => {
-    
+
     Product.find()
         .exec()
         .then(docs => {
+            // console.log(docs);
             const response = {
                 count: docs.length,
                 products: docs.map(doc => {
@@ -27,25 +29,30 @@ router.get('/', (req, res, next) => {
             res.status(200).json(response);
         })
         .catch(err => {
-            console.log(err);
-            res.status(500).json({
-                err:err
-            });
+           console.log(err);
+           res.status(500).json({
+              err: err
+           });
         });
+
 });
 
+
+
 router.post('/', (req, res, next) => {
+
     const product = new Product({
-        _id = new mongoose.Types.ObjectId(),
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
     });
+
     product
         .save()
         .then(result => {
             console.log(result);
             res.status(201).json({
-                message: 'Handling POST requests to /products',
+                message: "Created product successfully",
                 createdProduct: {
                     name: result.name,
                     price: result.price,
@@ -60,24 +67,26 @@ router.post('/', (req, res, next) => {
         .catch(err => {
             console.log(err);
             res.status(500).json({
-                errror:err
+                error: err
             });
         });
+
 });
+
 
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
-
     Product.findById(id)
+        .select('name price _id')
         .exec()
-        .then(docs => {
+        .then(doc => {
             console.log("From database", doc);
             if (doc) {
                 res.status(200).json({
                     product: doc,
                     request: {
                         type: "GET",
-                        url: "http://localhost:3000/products"
+                        url: 'http://localhost:3000/products'
                     }
                 });
             } else {
@@ -89,10 +98,14 @@ router.get('/:productId', (req, res, next) => {
         .catch(err => {
             console.log(err);
             res.status(500).json({
-                error:err
+                error: err
             });
-        });
+        })
 });
+
+
+
+
 
 router.patch('/:productId', (req, res, next) => {
     const id = req.params.productId;
@@ -100,7 +113,7 @@ router.patch('/:productId', (req, res, next) => {
     for (const ops of req.body) {
         updateOps[ops.propName] = ops.value;
     }
-    Product.update({ _id: id}, { $set: updateOps})
+    Product.update({ _id: id }, { $set: updateOps })
         .exec()
         .then(result => {
             res.status(200).json({
@@ -114,15 +127,18 @@ router.patch('/:productId', (req, res, next) => {
         .catch(err => {
             console.log(err);
             res.status(500).json({
-                error:err
+                error: err
             });
         });
 });
 
+
+
+
 router.delete('/:productId', (req, res, next) => {
 
     const id = req.params.productId;
-    Product.remove({ _id:id})
+    Product.remove({ _id: id })
         .exec()
         .then(result => {
             res.status(200).json({
@@ -130,17 +146,18 @@ router.delete('/:productId', (req, res, next) => {
                 request: {
                     type: 'POST',
                     url: 'http://localhost:3000/products',
-                    body: { name: 'String', price: 'Number'}
+                    body: { name: 'String', price: 'Number' }
                 }
             });
         })
         .catch(err => {
             console.log(err);
             res.status(500).json({
-                error:err
+                error: err
             });
         });
 });
+
 
 
 module.exports = router;
